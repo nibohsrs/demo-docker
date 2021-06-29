e9e2d83141f0187f636ed0c1994fc8532701f208
nibohsrs <shobin.srk@gmail.com>
2021-06-29 21:39:39 +0100

    first commit

---
+++ src/main/java/com/sumit/playjava/controller/MyController.java
@@ -0,0 +1,12 @@
+package com.sumit.playjava.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	@GetMapping("/test-docker")
+	public String getData() {
+		return "This is docker testing";
+	}
+}
